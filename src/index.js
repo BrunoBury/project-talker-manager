dafc69fe0038edc79030f67075d9228e570fd709
Bruno <bruno_bury@hotmail.com>
2023-09-03 14:37:38 -0300

    create endpoint GET/talker, req-01

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 app.use(express.json());
@@ -6,9 +8,24 @@ app.use(express.json());
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
 
+const TALKER_FILE = path.join(__dirname, 'talker.json');
+
 // não remova esse endpoint, e para o avaliador funcionar.
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+// Req 1;
+
+app.get('/talker', (_request, response) => {
+  const talkersJson = fs.readFileSync(TALKER_FILE, 'utf8');
+  const talkersData = JSON.parse(talkersJson);
+  
+  if (talkersData) {
+    response.status(HTTP_OK_STATUS).json(talkersData);
+  } else {
+    response.status(HTTP_OK_STATUS).json([]);
+  }
 });
 
 app.listen(PORT, () => {
