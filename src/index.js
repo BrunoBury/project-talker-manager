9775fc36021322d4096cf3d9070ac4a50c89f4f5
Bruno <bruno_bury@hotmail.com>
2023-09-04 14:15:17 -0300

    create endpoint id, req 02

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_NOT_FOUND_STATUS = 404;
 const PORT = process.env.PORT || '3001';
 
 const TALKER_FILE = path.join(__dirname, 'talker.json');
@@ -18,13 +19,29 @@ app.get('/', (_request, response) => {
 // Req 1;
 
 app.get('/talker', (_request, response) => {
-  const talkersJson = fs.readFileSync(TALKER_FILE, 'utf8');
+  const talkersJson = fs.readFile(TALKER_FILE, 'utf8');
   const talkersData = JSON.parse(talkersJson);
   
   if (talkersData) {
     response.status(HTTP_OK_STATUS).json(talkersData);
   } else {
     response.status(HTTP_OK_STATUS).json([]);
+  }
+});
+
+// Req 2;
+
+app.get('/talker/:id', (req, res) => {
+  const { id } = req.params;
+  const talkersJson = fs.readFile(TALKER_FILE, 'utf8');
+  const talkersData = JSON.parse(talkersJson);
+
+  const foundTalker = talkersData.find((talker) => talker.id === Number(id));
+
+  if (foundTalker) {
+    res.status(HTTP_OK_STATUS).json(foundTalker);
+  } else {
+    res.status(HTTP_NOT_FOUND_STATUS).json({ message: 'Pessoa palestrante não encontrada' });
   }
 });
 
